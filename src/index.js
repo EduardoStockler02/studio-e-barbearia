ea2186694f9eeac1e680c007178ee762696f38f2
Eduardo Stockler <eduardo.stockler@escola.pr.gov.br>
2025-04-03 10:29:16 -0300

    correcting some things

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
-$(document).ready(function () {
-    // Menu Toggle
-    $(".fa-bars").click(function () {
-        $(this).toggleClass("fa-times");
-        $("nav").toggleClass("nav-toggle");
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('nav').toggleClass('nav-toggle');
     });
-
-    // Animated Counting Effect
-    $(".count").each(function () {
-        var $this = $(this),
-        countTo = $this.attr("data-count");
-
-        $({ countNum: $this.text() }).animate(
-            { countNum: countTo },
-            {
-                duration: 5000,
-                step: function () {
-                    $this.text(Math.floor(this.countNum));
-                },
-                complete: function () {
-                    $this.text(this.countNum);
-                },
-            }
-        );
-    });
-
-    // Magnific Popup for Project Images
-    $(".projects").magnificPopup({
-        delegate: "a",
-        type: "image",
-        gallery: {
-            enabled: true,
+    $('.count').each(function(){
+        var $this=$(this),
+        countTo=$this.attr('data-count')
+        $({countNum: $this.text()}).animate({
+            countNum:countTo
         },
+        {
+        duration:5000,
+        step: function(){
+            $this.text(Math.floor(this.countNum));
+        },
+        complete: function(){
+                $this.text(this.countNum + '+')
+            }
+        }
+
+        )
     });
+    $('.recipes').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    }); 
 });
